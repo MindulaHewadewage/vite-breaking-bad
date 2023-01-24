ed73174b3c2bea82460212de262e87681319cb1c
MindulaHewadewage <mindufernando@gmail.com>
2023-01-24 18:41:55 +0100

    logo appheader

--- src/AppHeader.vue
+++ src/AppHeader.vue
@@ -5,7 +5,15 @@ export default {
 </script>
 
 <template>
-    <header></header>
+    <header>
+        <div class="pokedex-buttons">
+
+        </div>
+        <div class="pokemon-logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png"
+                alt="pokemon logo">
+        </div>
+    </header>
 </template>
 
 <style>
